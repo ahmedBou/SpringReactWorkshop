763b683185cc439a16a8ab3ec1d760c2a8c21b63
ahmed boutayeb <instinctauto@gmail.com>
2021-07-08 19:52:52 +0100

    playing with components

---
+++ frontend/todo-app/src/components/learning-examples/SecondComponent.jsx
@@ -0,0 +1,12 @@
+import {Component} from "react";
+
+
+export class SecondComponent extends Component{
+    render() {
+        return (
+            <div className="SecondComponent">
+                SecondComponent
+            </div>
+        );
+    }
+}
